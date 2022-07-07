cbd7130e92ec259e6777f4f576abc50621a76b5e
skourav <surbhimamar99@gmail.com>
2022-07-08 00:20:35 +0530

    test

--- Prac/src/Myclass.java
+++ Prac/src/Myclass.java
@@ -1,8 +1,23 @@
 
 public class Myclass{
 	
-	public static void main(String[] args){
+//	public static void main(String[] args){
+//	
+//	System.out.println("hi..");
+//	}
 	
-	System.out.println("hi..");
-	}
+	 public static void main(String args[]){  
+		   try{  
+		      //code that may raise exception  
+		      int data=100/0;  
+		   }catch(ArithmeticException e){
+			   
+//			   System.out.println(e);
+//			   e.printStackTrace();
+			   throw e;
+		   
+		   }  
+		   //rest code of the program   
+		   System.out.println("rest of the code...");  
+		  }  
 }
